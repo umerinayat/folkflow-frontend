d36ff4918d13de890817c6d66f2449c6cebbaa2f
Umer Inayat <umerinayat.369@gmail.com>
2020-11-08 14:49:07 +0500

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="jumbotron">
+      test
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  mounted() {
+    $('body').html('jquery rocks')
+  },
+  components: {}
+}
+</script>
+
